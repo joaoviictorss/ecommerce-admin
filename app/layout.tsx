e0c15bce39c7ef62bb58efc03e4bcf2234063249
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-05-14 10:53:43 -0300

    feat: adds the functionality to create stores

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 import { ModalProvider } from "@/providers/modal-provider";
+import prismadb from "@/lib/prismadb";
+import { ToastProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="pt-br">
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,57 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
+
 import { Modal } from "../ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(1, "O nome da loja é obrigatório"),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      window.location.assign(`/${response.data.id}`);
+      // utilizo o metodo assign no logar do sistema de rotas do next, pois ele da um refresh na pagina, que atualizará as lojas que foram criadas. Melhor UX
+    } catch (error) {
+      toast.error("Algo deu errado :(");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Modal
       title="Criar loja"
@@ -12,7 +59,41 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      TODO: criar formulario de loja
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="E-Commerce"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button
+                disabled={loading}
+                variant="outline"
+                onClick={storeModal.onClose}
+              >
+                Cancelar
+              </Button>
+              <Button disabled={loading} type="submit">
+                Continuar
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 };
